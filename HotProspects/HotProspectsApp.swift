8d17eac7b78326458aab6658c13a9895be853b81
AndresMarq <70426525+AndresMarq@users.noreply.github.com>
2021-08-15 12:42:57 -0600

    Initial Commit

---
+++ HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Andres Marquez on 2021-08-15.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
